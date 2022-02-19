9eafe5e36dd2a953cf98cc0505a118df918e61ed
Matt Wagner <wagnerm@alumni.uoguelph.ca>
2022-02-19 15:00:03 -0500

    402 solved

--- LeetCode402/Program.cs
+++ LeetCode402/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode402
 {
@@ -12,39 +13,58 @@ namespace LeetCode402
             Console.WriteLine("0" == RemoveKdigits("10", 1));
             Console.WriteLine("11" == RemoveKdigits("112", 1));
             Console.WriteLine("0" == RemoveKdigits("10001", 4));
+            Console.WriteLine("123" == RemoveKdigits("12345", 2));
+            Console.WriteLine("1111" == RemoveKdigits("1111111", 3));
+            Console.WriteLine("0" == RemoveKdigits("1234567890", 9));
         }
 
         static string RemoveKdigits(string num, int k)
         {
 
-            // loop through, if both sides lower (or an end) remove
+            var stack = new List<char>();
             int i = 0;
             while (k != 0)
             {
-                if (k == num.Length) return "0";
-                
-                if (i + 1 == num.Length)
+                if (k == num.Length)
                 {
-                    num = num.Remove(0, k);
-                    k = 0;
+                    return "0";
                 }
-                else if ((int)num[i] < (int)num[i + 1])
+                else if (i == num.Length)
                 {
-                    num = num.Remove(i + 1, 1);
-                    k--;
+                    while (k != 0)
+                    {
+                        stack.RemoveAt(stack.Count - 1);
+                        k--;
+                    }
                 }
-                else if ((int) num[i] == (int)num[i + 1])
+                else if (stack.Count == 0)
                 {
-                    i++;
+                    stack.Add(num[i]);
+                }
+                else if (stack[stack.Count - 1] > num[i])
+                {
+                    while (stack.Count != 0 && stack[stack.Count - 1] > num[i] && k != 0)
+                    {
+                        stack.RemoveAt(stack.Count - 1);
+                        k--;
+                    }
+
+                    stack.Add(num[i]);
                 }
                 else
                 {
-                    num = num.Remove(i, 1);
-                    k--;
+                    stack.Add(num[i]);
                 }
+                i++;
             }
 
-            num = RemoveLeadingZeros(num);
+            while(i < num.Length)
+            {
+                stack.Add(num[i]);
+                i++;
+            }
+
+            num = RemoveLeadingZeros(new string(stack.ToArray()));
 
             return num;
         }
