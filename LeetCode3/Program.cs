b711e61bae052a59541de3c192f3521c66cd6228
Matt Wagner <wagnerm@alumni.uoguelph.ca>
2022-03-09 10:28:56 -0500

    LeetCode #3

---
+++ LeetCode3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 == LengthOfLongestSubstring("abcabcbb"));
+        }
+        static int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            List<char> current = new List<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int offset = 0;
+
+                do
+                {
+                    current.Add(s[i + offset]);
+                    offset++;
+                } while (s.Length > i + offset && !current.Contains(s[i + offset]));
+
+                if (offset > result)
+                {
+                    result = offset;
+                }
+
+                current.Clear();
+            }
+
+            return result;
+        }
+    }
+}
